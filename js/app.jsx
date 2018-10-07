bb6484b8f95b8ee2efb981bf6a2b183d4bee8753
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-07 22:20:31 +0200

    done simple configuration

---
+++ js/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import '../scss/style.scss';
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('hi');
+  ReactDOM.render(<h1>Hello World!!!!</h1>, document.getElementById("app"));
+});
