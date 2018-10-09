3aa68c447e3b9a1f6b65cff5f8e0bfdaf4d4eee6
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-09 18:31:11 +0200

    done simply refactor

--- js/components/Recipe.jsx
+++ js/components/Recipe.jsx
@@ -1,6 +1,60 @@
 import React from "react";
 import recipesService from "../services/recipes.service";
 
+class Component extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="recipe-components">
+        <h3 className="h3">Główne składniki:</h3>
+        <div className="component">
+          {this.props.components.map(el => {
+            return (
+              <div key={el.id} className="component-item">
+                {el.name} {el.amount}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Steps extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+  }
+
+  showDesc = e => {
+    e.target.nextElementSibling.classList.toggle("step-description-show");
+  };
+
+  render() {
+    return (
+      <div className="recipe-steps">
+        <h3 className="h3">Sposób przygotowania:</h3>
+        {this.props.steps.map(el => {
+          return (
+            <div key={el.id}>
+              <div onClick={this.showDesc} className="step-title">
+                Krok {el.id} - {el.name}
+              </div>
+              <div className="step-description">{el.description}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
 export class Recipe extends React.Component {
   constructor(props) {
     super(props);
@@ -18,31 +72,8 @@ export class Recipe extends React.Component {
             <div className="list" key={el.id}>
               <h2 className="category-title">{el.name}</h2>
               <div className="recipe-container">
-                <div className="recipe-components">
-                  <h3 className="h3">Główne składniki:</h3>
-                  <div className="component">
-                    {this.state.components.map(el => {
-                      return (
-                        <div key={el.id} className="component-item">
-                          {el.name} {el.amount}
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-                <div className="recipe-steps">
-                  <h3 className="h3">Sposób przygotowania:</h3>
-                  <div>
-                    {this.state.steps.map(el => {
-                      return (
-                        <div key={el.id}>
-                          <div className='recipe-step-title'>{el.name}</div>
-                          <div className='recipe-step-description'>{el.description}</div>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
+                <Component components={this.state.components} />
+                <Steps steps={this.state.steps} />
               </div>
             </div>
           );
