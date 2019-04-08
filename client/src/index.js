bc32d63910e3e883e3206b87d67b3bb7043432c4
freestyle09 <jozefrzadkosz09@gmail.com>
2019-04-08 18:56:23 +0200

    Revert "fixed typescript problems and added styles"
    This reverts commit 8c87d63271fc79c012414b8a12b1841752c3e2e6.

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './components/App.tsx';
-
-import './styles/style.scss';
-
+import './index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
