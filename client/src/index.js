8c87d63271fc79c012414b8a12b1841752c3e2e6
freestyle09 <jozefrzadkosz09@gmail.com>
2019-04-08 18:05:56 +0200

    fixed typescript problems and added styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+
+import App from './components/App.tsx';
+
+import './styles/style.scss';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
